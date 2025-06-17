185abf45940ef47db1812b027706693dbd64da78
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2025-06-18 01:47:18 +0200

    vsinger covers pr

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "bandori",
-    title: "Bang Dream! Sorter",
+    localStoragePrefix: "vsinger-covers",
+    title: "VSinger Covers",
     description: "Party rank sorter for your custom list of songs."
 };
